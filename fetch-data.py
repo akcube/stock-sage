0f6f5bffb4a3ab44a19e447545240fef803af2b6
akcube <a.kishorekumar@students.iiit.ac.in>
2023-08-01 01:00:14 +0530

    Added redundancy for dealing with batch downloads & yfinance

--- fetch-data.py
+++ fetch-data.py
@@ -6,13 +6,16 @@ import contextlib
 from io import StringIO
 import shutil
 
+ERROR_LOG_FILE = 'error_log.txt'
+
 def fetch_market_data(tickers):
     '''
     Given a ticker/symbol list of active stocks on the NSE, fetch their price action and volume data since the beginning of time to data/{ticker}.csv
-    Default parallelization uses threads = 2*num_cpu_cores
+    Default parallelization uses threads = 2*num_cpu_cores. Sometimes fails because of the parallelization.
     :param tickers: The list of tickers/symbols to fetch data for
+    :return: A list containing the names of the failed stock tickers
     '''
-    failed_downloads = []
+    failed_downloads=[]
     try:
         # Annoying detail, yf does not throw an error on failure. Just outputs to stdout. 
         # Fix: Mute its output and raise an error ourselves
@@ -34,12 +37,8 @@ def fetch_market_data(tickers):
                 print(f'Downloaded market data for {ticker} and saved to {file_name}')
     except Exception as e:
         failed_downloads += tickers
-        
-    # Log errors to an error log
-    if failed_downloads:
-        with open('error_log.txt', 'a') as error_file:
-            for ticker in failed_downloads:
-                error_file.write(f"Failed to download market data for {ticker}\n")
+    finally:
+        return failed_downloads
 
 def main():
     # Fetch the csv containing all the ticker names
@@ -52,15 +51,29 @@ def main():
     # Create the data folder, clean it if it already exists
     shutil.rmtree('data', ignore_errors=True)
     os.makedirs('data', exist_ok=True)
+    if os.path.exists(ERROR_LOG_FILE):
+        os.remove(ERROR_LOG_FILE)
 
     # Split the valid tickers into smaller chunks for parallel processing
-    chunk_size = 200
+    chunk_size = 8
     ticker_chunks = [valid_tickers[i:i+chunk_size] for i in range(0, len(valid_tickers), chunk_size)]
 
-    # Fetch market data for each stock
+    # Fetch market data for each stock in chunks
+    failed_jobs = []
     for batch in ticker_chunks:
         tickers = [row['SYMBOL'].strip() for _, row in batch.iterrows()]
-        fetch_market_data(tickers)
+        failed_jobs += fetch_market_data(tickers)
+
+    # Try solo downloading the failed batches
+    fatal_failures = []
+    for ticker in failed_jobs:
+        fatal_failures += fetch_market_data(ticker)
+
+    # Log fatal failures
+    if fatal_failures:
+            with open(ERROR_LOG_FILE, 'a') as error_file:
+                for ticker in fatal_failures:
+                    error_file.write(f"Failed to download market data for {ticker}\n")
 
 if __name__ == '__main__':
     main()
